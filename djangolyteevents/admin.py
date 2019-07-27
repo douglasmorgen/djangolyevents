3dc278ca7bc53fdb694511ed8e1c2d7a8cb48952
Doug Morgen <douglasmorgen@gmail.com>
2019-07-27 14:34:28 -0700

    initial model

--- djangolyteevents/admin.py
+++ djangolyteevents/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+
+
 # Register your models here.
+from .models import Event
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display=['eventname','date']
--- djangolyteevents/models.py
+++ djangolyteevents/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+
+    eventname=models.CharField(max_length=255)
+    date = models.DateTimeField()
+    def __str__(self):
+        return "%s %s" % (self.eventname,self.date)
